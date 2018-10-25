65d42f52cc296755561f49191834613895116b61
Shota Shimazu <hornet.live.mf@gmail.com>
2018-10-25 09:37:00 +0900

    Initial Commit

---
+++ Coeur/Coeur.h
@@ -0,0 +1,19 @@
+//
+//  Coeur.h
+//  Coeur
+//
+//  Created by Shota Shimazu on 2018/10/25.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Coeur.
+FOUNDATION_EXPORT double CoeurVersionNumber;
+
+//! Project version string for Coeur.
+FOUNDATION_EXPORT const unsigned char CoeurVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coeur/PublicHeader.h>
+
+
