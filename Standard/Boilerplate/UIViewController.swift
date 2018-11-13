cbb19e008929ea4bbf8c1f8a6b4a6dfc5ca53fd4
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-13 23:11:19 +0900

    add new file

---
+++ Standard/Boilerplate/UIViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UIViewController.swift
+//  Standard
+//
+//  Created by Shota Shimazu on 2018/11/13.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
+import UIKit
+
+
+
+open class ViewController: UIViewController {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+        provideStyle()
+    }
+
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    public func provideStyle() {
+        
+    }
+}
