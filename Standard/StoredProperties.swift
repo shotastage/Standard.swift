2adab163f560c6c82968bbd247c7010998d5220e
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-14 12:41:10 +0900

    add screen size class

--- Standard/StoredProperties.swift
+++ Standard/StoredProperties.swift
@@ -16,3 +16,15 @@ public struct SafeArea {
     public static let top: CGFloat = UIApplication.shared.windows[0].safeAreaInsets.top
     public static let bottom: CGFloat = UIApplication.shared.windows[0].safeAreaInsets.bottom
 }
+
+
+open class ScreenSize {
+    
+    public static var width: CGFloat!
+    public static var height: CGFloat!
+    
+    init(view: UIView) {
+        ScreenSize.width = view.bounds.size.width
+        ScreenSize.height = view.bounds.size.height
+    }
+}
